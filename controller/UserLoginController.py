5dc8d5a7fe2ead4bab47908ea315f43643726163
why <529570509@qq.com>
2017-06-06 00:36:13 +0800

    env

--- controller/UserLoginController.py
+++ controller/UserLoginController.py
@@ -9,7 +9,7 @@ class UserLoginController(BaseController):
 	@service("UserService", "userService02", spec = "DEF")
 	def execute(self):
 		aes = AES(AES_KEY)
-		self.setResult(aes.encrypt("%s|%s|%d" % (TOKEN_HEADER, "123", int(time.time()))))
+		self.setResult(aes.encrypt("%s|%s|%d" % (TOKEN_HEADER, "123", int(time.time()))), msg = "OK")
 		print self.userService.findUser("111", "111")
 		print self.userService02.findUser("111", "111")
 
