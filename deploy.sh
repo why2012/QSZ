4e6e9bd30ac72a29e6db40732e3a430ea854bca4
why <529570509@qq.com>
2017-11-29 19:21:43 +0800

    rename docker name

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # docker run -d -v /QSZ/mysqldata:/var/lib/mysql -p 3306:3306 -e MYSQL_ROOT_PASSWORD=qingsongzumysqlserver2017. --name mysql-server daocloud.io/library/mysql
-# sudo docker run -it -d -v /:/workdir -p 20001:20001 -p 20002:20002 --name tornado-env tornado-env:0.1 bash -c "cd /workdir/QSZ/QSZ && chmod a+x deploy.sh && ./deploy.sh online"
+# sudo docker run -it -d -v /:/workdir -p 20001:20001 -p 20002:20002 --name qsz tornado-env:0.1 bash -c "cd /workdir/QSZ/QSZ && chmod a+x deploy.sh && ./deploy.sh online"
 case "$@" in
 	online)
 		sed -i "s/DEV = True/DEV = False/g" conf/Config.py
@@ -8,13 +8,13 @@ case "$@" in
 		python MainApplication.py
 		;;
 	dev-deploy)
-		ssh -t why@119.29.113.28 "bash -c 'cd /QSZ/QSZ && git branch && git stash && git pull && sudo docker restart tornado-env'"
+		ssh -t why@119.29.113.28 "bash -c 'cd /QSZ/QSZ && git branch && git stash && git pull && sudo docker restart qsz'"
 		;;
 	online-deploy)
-		ssh -t why@119.29.113.28 "bash -c 'cd /QSZ/QSZ && git checkout master && git branch && git stash && git pull && sudo docker restart tornado-env'"
+		ssh -t why@119.29.113.28 "bash -c 'cd /QSZ/QSZ && git checkout master && git branch && git stash && git pull && sudo docker restart qsz'"
 		;;
 	rollback)
-		ssh -t why@119.29.113.28 "bash -c 'cd /QSZ/QSZ && git reset --hard HEAD^ && sudo docker restart tornado-env'"
+		ssh -t why@119.29.113.28 "bash -c 'cd /QSZ/QSZ && git reset --hard HEAD^ && sudo docker restart qsz'"
 		;;
 	*)
 		echo "dev-deploy online-deploy rollback"
